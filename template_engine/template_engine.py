b7b14829ffcc64ab973cfc0e56368cb8c7c3d7c9
Heiko Burau <burau@pks.mpg.de>
2020-10-29 12:41:06 +0100

    add template engine

---
+++ template_engine/template_engine.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+from itertools import product
+import json
+import sys
+
+
+BEGIN_TEMPLATE = "//! begin template"
+END_TEMPLATE = "//! end template"
+
+
+with open(sys.argv[1]) as f:
+    config = json.load(f)
+
+file_patterns = config["file_patterns"]
+keywords = config["keywords"]
+
+root = Path(sys.argv[2])
+
+
+def generate_output(line_iter):
+    while True:
+        try:
+            line = next(line_iter)
+        except StopIteration:
+            return
+
+        if BEGIN_TEMPLATE in line:
+            template = list(read_template(line_iter))
+            yield from expand_template(template)
+        else:
+            yield line
+
+
+def read_template(line_iter):
+    while True:
+        line = next(line_iter)
+        if END_TEMPLATE in line:
+            return
+        yield line
+
+
+def expand_template(template):
+    matching_keywords = {
+        keyword: instances for keyword, instances in keywords.items()
+        if any(keyword in line for line in template)
+    }
+
+    for instances in product(*matching_keywords.values()):
+        yield r"#if " + " && ".join(f"defined({instance['flag']})" for instance in instances) + "\n"
+
+        for line in template:
+            for keyword, instance in zip(matching_keywords, instances):
+                line = line.replace(keyword, instance["code"])
+
+            yield line
+
+        yield "#endif\n"
+
+
+for file_pattern in file_patterns:
+    for file_path in root.glob(file_pattern):
+        print(file_path)
+
+        with open(file_path) as f:
+            lines = f.readlines()
+
+        output = list(generate_output(iter(lines)))
+
+        with open(str(file_path)[:-len(file_path.suffix)], 'w') as f:
+            f.writelines(output)
