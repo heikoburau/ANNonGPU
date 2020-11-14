0f00c1cdfd24ad99816e50f41ec4ac27be81ab3e
Heiko Burau <burau@pks.mpg.de>
2020-11-14 16:38:11 +0100

    adapt main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,26 +1,31 @@
 #include "quantum_states.hpp"
-#include "ensembles/MonteCarlo.hpp"
+// #include "ensembles/MonteCarlo.hpp"
+#include "ensembles/ExactSummation.hpp"
 #include "operator/Operator.hpp"
 // #include "network_functions/HilbertSpaceDistance.hpp"
+#include "network_functions/ExpectationValue.hpp"
 #include "types.h"
 
 #include "QuantumExpression/QuantumExpression.hpp"
 
 #include <cuda_profiler_api.h>
 
+#include <map>
 #include <iostream>
 #include <complex>
 #include <vector>
+#include <chrono>
 
 
 using namespace std;
+using namespace std::chrono;
 using namespace ann_on_gpu;
 using namespace quantum_expression;
 using cpx = complex<double>;
 using imap = map<int, int>;
 
-const unsigned int N = 128;
-const unsigned int M = 128;
+const unsigned int N = 14;
+const unsigned int M = 14;
 const bool gpu = true;
 
 int main(int argc, char *argv[]) {
@@ -28,6 +33,7 @@ int main(int argc, char *argv[]) {
 
     for(auto i = 0; i < (int)N; i++) {
         H += PauliExpression(imap{{i, 3}, {(i + 1) % N, 3}});
+        H += PauliExpression(imap{{i, 2}, {(i + 1) % N, 2}});
         H += PauliExpression(imap{{i, 1}});
     }
     const auto U = cpx(0.0, -1.0) * H * 0.1;
@@ -37,22 +43,36 @@ int main(int argc, char *argv[]) {
     PsiDeep psi(N, M, gpu);
 
     // HilbertSpaceDistance hs_distance(psi.num_params, gpu);
+    ExpectationValue evalue(gpu);
 
     // MonteCarloSpins mc_loop(
-    //     1u << 14u,
+    //     1u << 12u,
     //     1u,
     //     2u,
     //     gpu ? (1u << 10u) : 1u,
+    //     Update_Policy<ann_on_gpu::Spins>(),
     //     gpu
     // );
 
-    vector<cpx> result(psi.num_params);
+    ExactSummationSpins esum(N, gpu);
 
-    cudaProfilerStart();
+    // vector<cpx> result(psi.num_params);
 
-    // cout << hs_distance.gradient(result.data(), psi, psi, op, false, mc_loop, 0.0) << endl;
+    // cudaProfilerStart();
 
-    cudaProfilerStop();
+    const auto start = high_resolution_clock::now();
+
+    cout << evalue(op, psi, esum) << endl;
+
+    const auto end = high_resolution_clock::now();
+
+    // // cout << hs_distance.gradient(result.data(), psi, psi, op, false, mc_loop, 0.0) << endl;
+
+    // cudaProfilerStop();
+
+    duration<double, milli> fp_ms = end - start;
+
+    cout << "duration: " << fp_ms.count() << endl;
 
     return 0;
 }
