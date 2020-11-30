771ebf22c88e29739eb341156376b799dea17baa
Heiko Burau <burau@pks.mpg.de>
2020-11-30 14:09:50 +0100

    tests for apply_operator pass

--- test/test_operator.py
+++ test/test_operator.py
@@ -1,55 +1,20 @@
-from pyANNonGPU import LearningByGradientDescent
-from math import acos, sqrt
+from pyANNonGPU import apply_operator, Operator
+from pytest import approx
 
 
-def two_state_probability(psi, phi):
-    psi_dot_phi = psi.conj() @ phi
-    return min(
-        (psi_dot_phi * psi_dot_phi.conj() / ((psi.conj() @ psi) * (phi.conj() @ phi))).real,
-        1.0
-    )
+def test_operator(psi_all, all_operators, ensemble, gpu):
+    psi = psi_all(gpu)
 
-
-def fubini_study(psi, phi):
-    return acos(sqrt(two_state_probability(psi, phi)))
-
-
-def test_operator(psi_deep, single_sigma, ensemble, gpu):
-    psi_0 = psi_deep(gpu)
-
-    num_sites = psi_0.num_sites
-
-    use_spins = ensemble.__name__.endswith("Spins")
-    if not use_spins and psi_0.N != 3 * num_sites:
-        return
+    num_sites = psi.num_sites
 
     ensemble = ensemble(num_sites, gpu)
+    if ensemble.__class__.__name__.startswith("ExactSummation"):
+        psi.normalize(ensemble)
 
-    op = single_sigma(num_sites)
-    if psi_0.symmetric:
-        op = sum(op.roll(i, num_sites) for i in range(num_sites))
+    expr = all_operators(num_sites)
+    op = Operator(expr, psi.gpu)
 
-    psi_0.calibrate(ensemble)
+    vector = psi.vector(ensemble)
+    vector_prime = apply_operator(psi, op, ensemble)
 
-    # print("L:", num_sites)
-    # print("operator:", op)
-
-    learning = LearningByGradientDescent(psi_0.num_params, ensemble)
-
-    U = 1 + 0.1j * op
-
-    psi_out = learning.optimize_for(psi_0, psi_0, U, True, eta=1e-2)
-
-    assert fubini_study(
-        U.matrix(num_sites, "spins" if use_spins else "paulis") @ psi_0.vector(ensemble),
-        psi_out.vector(ensemble)
-    ) < 1e-2
-
-    U = 1 + 0.1 * op
-
-    psi_out = learning.optimize_for(psi_0, psi_0, U, True, eta=1e-2)
-
-    assert fubini_study(
-        U.matrix(num_sites, "spins" if use_spins else "paulis") @ psi_0.vector(ensemble),
-        psi_out.vector(ensemble)
-    ) < 1e-2
+    assert approx(expr @ vector, vector_prime)
