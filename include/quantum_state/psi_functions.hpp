877fc6520ec809d5df7b82d25a1640336f9d3442
Heiko Burau <burau@pks.mpg.de>
2021-02-19 20:07:58 +0100

    use Pade(2,4) also for deep layers

--- include/quantum_state/psi_functions.hpp
+++ include/quantum_state/psi_functions.hpp
@@ -10,35 +10,74 @@ constexpr auto b = 1.0;
 template<typename T>
 HDINLINE
 T deep_activation(const T z) {
+    using scalar = typename T::value_type;
     // return complex_t(1.0, 0.0) * z * log(cosh(z));
-    return tanh(z);
+    // return tanh(z);
+
+    const auto sign = z.real() > scalar(0.0) ? scalar(1.0) : scalar(-1.0);
+
+    // return sign * z + (1.81168 - sign * 1.22741 * z) / (2.61371 + z * (sign * 2.0 + z)) - 0.693147;
+
+    return sign * scalar(0.9003320053750442) * z + (
+        scalar(5.49914721954) - sign * scalar(2.16564366435) * z
+    ) / (
+        scalar(9.19376335670885) + z * (sign * scalar(10.2180213465) + z * (scalar(7.771429504240965) + z * (sign * scalar(3.746646023906276) + z)))
+    ) - scalar(0.598139);
 }
 
 template<typename T>
 HDINLINE
 T deep_activation_diff(const T z) {
+    using scalar = typename T::value_type;
+
     // return complex_t(1.0, 0.0) * (
     //     log(cosh(z)) + z * tanh(z)
     // );
-    const auto co = cosh(z);
-    return complex_t(1.0) / (co * co);
+    // const auto co = cosh(z);
+    // return complex_t(1.0) / (co * co);
+
+    const auto sign = z.real() > scalar(0.0) ? scalar(1.0) : scalar(-1.0);
+    // const auto denominator = 2.61371 + z * (sign * 2.0 + z);
+    // return (
+    //     z * (6.83146 + z * (sign * 10.4548 + z * (4.0 + sign * z)))
+    // ) / (denominator * denominator);
+
+    const auto denominator = scalar(9.19376335670885) + z * (sign * scalar(10.218021346543315) + z * (scalar(7.771429504240965) + z * (sign * scalar(3.746646023906276) + z)));
+    return (
+        z * (
+            scalar(83.68563506532087) + z * (
+                sign * scalar(177.6769746361748) + z * (
+                    scalar(199.24474920889975) + z * (
+                        sign * scalar(146.36284300074402) + z * (
+                            scalar(70.82878897882324) + z * (
+                                sign * scalar(26.632014683761202) + z * (
+                                    scalar(6.746450656267947) + sign * scalar(0.9003320053750442) * z
+        )))))))
+    ) / (denominator * denominator);
 }
 
 template<>
 HDINLINE
 double deep_activation(const double z) {
+
     // return complex_t(1.0, 0.0) * z * log(cosh(z));
     return tanh(z);
+
+
 }
 
 template<>
 HDINLINE
 double deep_activation_diff(const double z) {
+    // using scalar = typename T::value_type;
+
     // return complex_t(1.0, 0.0) * (
     //     log(cosh(z)) + z * tanh(z)
     // );
     const auto co = cosh(z);
     return 1.0 / (co * co);
+
+
 }
 
 template<typename T>
