ffec32c5201e0ec2186b4627ac430828983d66eb
Heiko Burau <burau@pks.mpg.de>
2021-03-16 16:38:52 +0100

    use Taylor(6) activation functions

--- include/quantum_state/psi_functions.hpp
+++ include/quantum_state/psi_functions.hpp
@@ -5,80 +5,6 @@
 
 namespace ann_on_gpu {
 
-constexpr auto b = 1.0;
-
-template<typename T>
-HDINLINE
-T deep_activation(const T z) {
-    using scalar = typename T::value_type;
-    // return complex_t(1.0, 0.0) * z * log(cosh(z));
-    // return tanh(z);
-
-    const auto sign = z.real() > scalar(0.0) ? scalar(1.0) : scalar(-1.0);
-
-    // return sign * z + (1.81168 - sign * 1.22741 * z) / (2.61371 + z * (sign * 2.0 + z)) - 0.693147;
-
-    return sign * scalar(0.9003320053750442) * z + (
-        scalar(5.49914721954) - sign * scalar(2.16564366435) * z
-    ) / (
-        scalar(9.19376335670885) + z * (sign * scalar(10.2180213465) + z * (scalar(7.771429504240965) + z * (sign * scalar(3.746646023906276) + z)))
-    ) - scalar(0.598139);
-}
-
-template<typename T>
-HDINLINE
-T deep_activation_diff(const T z) {
-    using scalar = typename T::value_type;
-
-    // return complex_t(1.0, 0.0) * (
-    //     log(cosh(z)) + z * tanh(z)
-    // );
-    // const auto co = cosh(z);
-    // return complex_t(1.0) / (co * co);
-
-    const auto sign = z.real() > scalar(0.0) ? scalar(1.0) : scalar(-1.0);
-    // const auto denominator = 2.61371 + z * (sign * 2.0 + z);
-    // return (
-    //     z * (6.83146 + z * (sign * 10.4548 + z * (4.0 + sign * z)))
-    // ) / (denominator * denominator);
-
-    const auto denominator = scalar(9.19376335670885) + z * (sign * scalar(10.218021346543315) + z * (scalar(7.771429504240965) + z * (sign * scalar(3.746646023906276) + z)));
-    return (
-        z * (
-            scalar(83.68563506532087) + z * (
-                sign * scalar(177.6769746361748) + z * (
-                    scalar(199.24474920889975) + z * (
-                        sign * scalar(146.36284300074402) + z * (
-                            scalar(70.82878897882324) + z * (
-                                sign * scalar(26.632014683761202) + z * (
-                                    scalar(6.746450656267947) + sign * scalar(0.9003320053750442) * z
-        )))))))
-    ) / (denominator * denominator);
-}
-
-template<>
-HDINLINE
-double deep_activation(const double z) {
-
-    // return complex_t(1.0, 0.0) * z * log(cosh(z));
-    return tanh(z);
-
-
-}
-
-template<>
-HDINLINE
-double deep_activation_diff(const double z) {
-    // using scalar = typename T::value_type;
-
-    // return complex_t(1.0, 0.0) * (
-    //     log(cosh(z)) + z * tanh(z)
-    // );
-    const auto co = cosh(z);
-    return 1.0 / (co * co);
-
-
-}
 
 template<typename T>
 HDINLINE
@@ -104,12 +30,20 @@ T my_logcosh(const T z, const unsigned int layer) {
     // return complex_t(0.0, -1.0) * log(cosh(z)) + complex_t(0.0, -0.346574);
 
     // for TDVP it is important to use log cosh, or at least not Pade(2, 4)
+    // if(layer == 0u) {
+    //     return log(cosh(z));
+    // }
+    // else {
+    //    return tanh(z);
+    // }
+
+    const auto z2 = z * z;
+    const auto z4 = z2 * z2;
+
     if(layer == 0u) {
-        return log(cosh(z));
+        return 0.5 * z2 - (1.0 / 12.0) * z4 + (1.0 / 45.0) * z4 * z2;
     }
-    else {
-       return tanh(z);
-    }
+    return z - (1.0 / 3.0) * z2 * z + (2.0 / 15.0) * z4 * z;
 
     // seems to be dangerous. Does not work for a SW-generator applied on an initial state.
     // return log(1.0 + z*z);
@@ -163,13 +97,18 @@ T my_tanh(const T z, const unsigned int layer) {
 
     // return z / sqrt(1.0 + z*z);
 
+    const auto z2 = z * z;
+    const auto z4 = z2 * z2;
+
     // for TDVP it is important to use tanh, or at least not Pade(2, 4)
     if(layer == 0u) {
-        return tanh(z);
+        // return tanh(z);
+        return z - (1.0 / 3.0) * z2 * z + (2.0 / 15.0) * z4 * z;
     }
     else {
-        const auto co = cosh(z);
-        return 1.0 / (co * co);
+        // const auto co = cosh(z);
+        // return 1.0 / (co * co);
+        return 1.0 - z2 + (2.0 / 3.0) * z4;
     }
 
     // const auto e_z = exp(z);
@@ -205,28 +144,5 @@ T my_tanh(const T z, const unsigned int layer) {
     //     )))))))
     // ) / (denominator * denominator);
 }
-
-
-HDINLINE double my_logcosh(const double& x) {
-    // if (x > 0.0) {
-    //     return x;
-    // }
-    // return 0.0;
-    return log(cosh(x));
-    // return tanh(x);
-    // return x;
-}
-
-HDINLINE double my_tanh(const double& x) {
-    // return 1.0;
-    return tanh(x);
-    // const auto y = cosh(x);
-    // return 1.0 / (y * y);
-    // if (x > 0.0) {
-    //     return 1.0;
-    // }
-    // return 0.0;
-}
-
 
 } // namespace ann_on_gpu
