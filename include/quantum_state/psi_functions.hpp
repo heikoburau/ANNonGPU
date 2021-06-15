c73389b6d8966cfe35c19406656d45998f14ad4e
Heiko Burau <burau@pks.mpg.de>
2021-06-15 17:34:20 +0200

    use taylored logcosh as activation function

--- include/quantum_state/psi_functions.hpp
+++ include/quantum_state/psi_functions.hpp
@@ -9,7 +9,7 @@ namespace ann_on_gpu {
 template<typename T>
 HDINLINE
 T my_logcosh(const T z, const unsigned int layer) {
-    using scalar = typename T::value_type;
+    // using scalar = typename T::value_type;
 
     // const auto r = abs(z);
     // if(r < b) {
@@ -37,15 +37,15 @@ T my_logcosh(const T z, const unsigned int layer) {
     //    return tanh(z);
     // }
 
-    // const auto z2 = z * z;
-    // const auto z4 = z2 * z2;
+    const auto z2 = z * z;
+    const auto z4 = z2 * z2;
 
-    // if(layer == 0u) {
-    //     // return 0.5 * z2 + (1.0 / 6.0) * z2 * z - (1.0 / 12.0) * z4 + (1.0 / 45.0) * z4 * z2;
-    //     return 0.5 * z2 - (1.0 / 12.0) * z4 + (1.0 / 45.0) * z4 * z2;
-    //     // return -(1.0 / 3.0) * z2 * z + (2.0 / 15.0) * z4 * z;
-    // }
-    // return z - (1.0 / 3.0) * z2 * z + (2.0 / 15.0) * z4 * z;
+    if(layer == 0u) {
+        // return 0.5 * z2 + (1.0 / 6.0) * z2 * z - (1.0 / 12.0) * z4 + (1.0 / 45.0) * z4 * z2;
+        return 0.5 * z2 - (1.0 / 12.0) * z4 + (1.0 / 45.0) * z4 * z2;
+        // return -(1.0 / 3.0) * z2 * z + (2.0 / 15.0) * z4 * z;
+    }
+    return z - (1.0 / 3.0) * z2 * z + (2.0 / 15.0) * z4 * z;
 
     // seems to be dangerous. Does not work for a SW-generator applied on an initial state.
     // return log(1.0 + z*z);
@@ -64,21 +64,21 @@ T my_logcosh(const T z, const unsigned int layer) {
     // return log((exp(z) - 1.0) / z);
 
     // Pade(2, 4) is good for compression
-    const auto sign = z.real() > scalar(0.0) ? scalar(1.0) : scalar(-1.0);
+    // const auto sign = z.real() > scalar(0.0) ? scalar(1.0) : scalar(-1.0);
 
-    // return sign * z + (1.81168 - sign * 1.22741 * z) / (2.61371 + z * (sign * 2.0 + z)) - 0.693147;
+    // // return sign * z + (1.81168 - sign * 1.22741 * z) / (2.61371 + z * (sign * 2.0 + z)) - 0.693147;
 
-    return sign * scalar(0.9003320053750442) * z + (
-        scalar(5.49914721954) - sign * scalar(2.16564366435) * z
-    ) / (
-        scalar(9.19376335670885) + z * (sign * scalar(10.2180213465) + z * (scalar(7.771429504240965) + z * (sign * scalar(3.746646023906276) + z)))
-    ) - scalar(0.598139);
+    // return sign * scalar(0.9003320053750442) * z + (
+    //     scalar(5.49914721954) - sign * scalar(2.16564366435) * z
+    // ) / (
+    //     scalar(9.19376335670885) + z * (sign * scalar(10.2180213465) + z * (scalar(7.771429504240965) + z * (sign * scalar(3.746646023906276) + z)))
+    // ) - scalar(0.598139);
 }
 
 template<typename T>
 HDINLINE
 T my_tanh(const T z, const unsigned int layer) {
-    using scalar = typename T::value_type;
+    // using scalar = typename T::value_type;
     // const auto r = abs(z);
     // if(r < b) {
     //     return complex_t(0.0, 0.0);
@@ -99,21 +99,21 @@ T my_tanh(const T z, const unsigned int layer) {
 
     // return z / sqrt(1.0 + z*z);
 
-    // const auto z2 = z * z;
-    // const auto z4 = z2 * z2;
+    const auto z2 = z * z;
+    const auto z4 = z2 * z2;
 
-    // // for TDVP it is important to use tanh, or at least not Pade(2, 4)
-    // if(layer == 0u) {
-    //     // return tanh(z);
-    //     // return z + 0.5 * z2 - (1.0 / 3.0) * z2 * z + (2.0 / 15.0) * z4 * z;
-    //     return z - (1.0 / 3.0) * z2 * z + (2.0 / 15.0) * z4 * z;
-    //     // return -z2 + (2.0 / 3.0) * z4;
-    // }
-    // else {
-    //     // const auto co = cosh(z);
-    //     // return 1.0 / (co * co);
-    //     return 1.0 - z2 + (2.0 / 3.0) * z4;
-    // }
+    // for TDVP it is important to use tanh, or at least not Pade(2, 4)
+    if(layer == 0u) {
+        // return tanh(z);
+        // return z + 0.5 * z2 - (1.0 / 3.0) * z2 * z + (2.0 / 15.0) * z4 * z;
+        return z - (1.0 / 3.0) * z2 * z + (2.0 / 15.0) * z4 * z;
+        // return -z2 + (2.0 / 3.0) * z4;
+    }
+    else {
+        // const auto co = cosh(z);
+        // return 1.0 / (co * co);
+        return 1.0 - z2 + (2.0 / 3.0) * z4;
+    }
 
     // const auto e_z = exp(z);
     // return e_z + (1.0 + e_z);
@@ -129,24 +129,24 @@ T my_tanh(const T z, const unsigned int layer) {
     // return -1.0 / z + exp_z / (exp_z - 1.0);
 
     // Pade(2, 4) is good for compression
-    const auto sign = z.real() > scalar(0.0) ? scalar(1.0) : scalar(-1.0);
-    // const auto denominator = 2.61371 + z * (sign * 2.0 + z);
-    // return (
-    //     z * (6.83146 + z * (sign * 10.4548 + z * (4.0 + sign * z)))
-    // ) / (denominator * denominator);
+    // const auto sign = z.real() > scalar(0.0) ? scalar(1.0) : scalar(-1.0);
+    // // const auto denominator = 2.61371 + z * (sign * 2.0 + z);
+    // // return (
+    // //     z * (6.83146 + z * (sign * 10.4548 + z * (4.0 + sign * z)))
+    // // ) / (denominator * denominator);
 
-    const auto denominator = scalar(9.19376335670885) + z * (sign * scalar(10.218021346543315) + z * (scalar(7.771429504240965) + z * (sign * scalar(3.746646023906276) + z)));
-    return (
-        z * (
-            scalar(83.68563506532087) + z * (
-                sign * scalar(177.6769746361748) + z * (
-                    scalar(199.24474920889975) + z * (
-                        sign * scalar(146.36284300074402) + z * (
-                            scalar(70.82878897882324) + z * (
-                                sign * scalar(26.632014683761202) + z * (
-                                    scalar(6.746450656267947) + sign * scalar(0.9003320053750442) * z
-        )))))))
-    ) / (denominator * denominator);
+    // const auto denominator = scalar(9.19376335670885) + z * (sign * scalar(10.218021346543315) + z * (scalar(7.771429504240965) + z * (sign * scalar(3.746646023906276) + z)));
+    // return (
+    //     z * (
+    //         scalar(83.68563506532087) + z * (
+    //             sign * scalar(177.6769746361748) + z * (
+    //                 scalar(199.24474920889975) + z * (
+    //                     sign * scalar(146.36284300074402) + z * (
+    //                         scalar(70.82878897882324) + z * (
+    //                             sign * scalar(26.632014683761202) + z * (
+    //                                 scalar(6.746450656267947) + sign * scalar(0.9003320053750442) * z
+    //     )))))))
+    // ) / (denominator * denominator);
 }
 
 } // namespace ann_on_gpu
